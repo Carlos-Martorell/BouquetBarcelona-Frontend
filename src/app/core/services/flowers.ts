c4ced913380528de18348f07dea717d122dce26d
Carlos Martorell <cmartorellotal@gmail.com>
2025-11-15 10:54:05 +0100

    feat(api): Implement Products Service for CRUD operations
    Created the ProductsService in Angular to handle API integration with the NestJS backend.
    Configured HttpClient and Observable handling for all necessary CRUD endpoints (GET, POST, PUT, DELETE)

--- src/app/core/services/flowers.ts
+++ src/app/core/services/flowers.ts
@@ -1,8 +1,50 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, signal } from '@angular/core';
+import { CreateFlower, Flower, UpdateFlower } from '../models/flower';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Flowers {
+export class FlowersService {
+
+private readonly apiUrl = 'http://localhost:3000/api/flowers'
+
+private flowersSignal = signal<Flower[]> ([])
+
+readonly flowers = this.flowersSignal.asReadonly()
+
+constructor(private http:HttpClient) {}
+
+getAll(): Observable<Flower[]> { 
+  return this.http.get<Flower[]>(this.apiUrl).pipe(
+    tap(flowers => this.flowersSignal.set(flowers))
+  )
+}
   
+getOne(id: string): Observable<Flower>{
+  return this.http.get<Flower>(`${this.apiUrl}/${id}`)
+}
+
+create(flower: CreateFlower): Observable<Flower>{
+  return this.http.post<Flower>(this.apiUrl, flower).pipe(
+    tap(newFlower => this.flowersSignal.update(flowers => [...flowers, newFlower]))
+  )
+}
+
+update(id: string, flower: UpdateFlower): Observable<Flower>{
+  return this.http.patch<Flower>(`${this.apiUrl}/${id}`, flower).pipe(
+    tap(updateFlower => this.flowersSignal.update(flowers => flowers.map(
+      f => f._id === id ? updateFlower : f
+    )))
+  )
+}
+
+delete(id: string): Observable<void>{
+  return this.http.delete<void>(`${this.apiUrl}/${id}`).pipe(
+    tap(() =>
+      this.flowersSignal.update(flower => flower.filter(f => f._id !== id))
+    )
+  )}
+
 }
